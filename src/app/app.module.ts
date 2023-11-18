b68e220dca90df4c48d1531d23d08c17accbe3ea
unknown <rafael.eng.mecanic@gmail.com>
2023-11-18 00:52:44 -0300

    style bottom sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SettingsComponent } from './settings/settings.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    SettingsComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
